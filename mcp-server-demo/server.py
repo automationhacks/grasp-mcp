16713ff3bcb44d15a81ae5044ca4568485982446
automationhacks <automationhacks@gmail.com>
2025-05-21 21:59:22 +0530

    Add client implementation and review_code prompt to server

--- mcp-server-demo/server.py
+++ mcp-server-demo/server.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from mcp.server.fastmcp import FastMCP
 
 
@@ -12,3 +14,12 @@ def add(a: int, b: int) -> int:
 @mcp.resource("greeting://{name}")
 def get_greeting(name: str) -> str:
     return f"Hello, {name}!"
+
+
+@mcp.prompt()
+def review_code(code : str) -> str:
+    return f"Please review the following code:\n\n{code}"
+
+
+if __name__ == "__main__":
+    asyncio.run(mcp.run())
